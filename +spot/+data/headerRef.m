a20937f62b504db0b02db4ab0e3d5e4438234454
Thomas <yuyanglai@yahoo.com>
2012-09-10 11:29:10 -0700

    completed headerRef

--- +spot/+data/headerRef.m
+++ +spot/+data/headerRef.m
@@ -2,13 +2,43 @@ function h = headerRef(header,index)
 %HEADERREF  "Subsref" function for SeisDataContainer headers
 %
 %   h = headerRef(header,index) basically returns a header with a subsrefed
-%   range of its header entities
+%   range (in the dimensional sense) of its header entities
 
-assert(isnumeric(index), 'index must be a numeric vector!');
+% Index checking
+assert(isnumeric(index), 'index must be numeric!');
+assert(isvector(index), 'index must be a vector!');
 
-dims   = header.dims;
-size   = header.size;
-origin = header.origin;
-delta  = header.delta;
-unit   = header.unit;
-label  = header.label;
+size    = header.size;
+origin  = header.origin;
+delta   = header.delta;
+unit    = header.unit;
+label   = header.label;
+
+% Check and remove singleton dimension
+assert(length(origin) > 1, 'dims have to be at least 2');
+if length(size) == length(origin) + 1
+    assert(size(end) == 1, 'ERROR: sizes dimension mismatch check header');
+    size = size(1:end-1);
+else
+    assert(length(size) == length(origin), 'ERROR: sizes mismatch check header');
+end
+
+% Check index range
+assert(index(1) >= 1 && index(end) < length(origin), 'index out of bounds');
+
+% Fill in singleton dimension for vec case
+if isscalar(size)
+    size = [size 1];
+end
+
+% Update dims
+dims = length(size(index));
+
+% Fill in the new ranges
+h        = header;
+h.dims   = dims;
+h.size   = size(index);
+h.origin = origin(index);
+h.delta  = delta(index);
+h.unit   = unit(index);
+h.label  = label(index);
