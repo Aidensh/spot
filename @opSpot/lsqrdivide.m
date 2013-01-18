d7e254562026b4c5c5fb6e1bf7882d97a3088799
Thomas <yuyanglai@yahoo.com>
2013-01-18 14:04:11 -0800

    [FIX] Fix preallocation bug for lsqrdivide
    The size of the preallocated x was totally wrong, instead of x = zeros(size(b)) it should have been x = zeros(size(A,2), size(b,2))

--- @opSpot/lsqrdivide.m
+++ @opSpot/lsqrdivide.m
@@ -9,7 +9,7 @@ opts = spotparams;
 maxits = opts.cgitsfact * min(m,min(n,20));
 
 % Preallocate x
-x = zeros(size(b));
+x = zeros(size(A,2),size(b,2));
 for j=1:size(b,2)
     x(:,j) = spot.solvers.lsqr(m,n,A,b(:,j),opts.cgdamp,opts.cgtol,...
         opts.cgtol,opts.conlim,maxits,opts.cgshow);
